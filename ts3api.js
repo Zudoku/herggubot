4d3fbe275cbb36d2b85e463a9f33f932f4849616
Rivenation <rivenation@gmail.com>
2015-09-26 16:25:49 +0300

    More ts functions to ts3api

--- ts3api.js
+++ ts3api.js
@@ -28,6 +28,50 @@ module.exports = {
 	registerListener: function (listenerName, listener) {
 		this.__client.on(listenerName, listener);
 	},
+	sendClientMessage: function (targetClientId, message, callback) {
+		this.__sendCommand("sendtextmessage", { targetmode: 1, target: targetClientId, msg: message }, function (err, res) {
+			if (typeof callback == "function")
+				return callback(err, res);
+		});
+	},
+	getClientsInChannel: function (channelId, callback) {
+		this.__sendCommand("clientlist", {}, function (err, res) {
+			if (err && typeof callback == "function")
+				return callback(err);
+			var targetClients = [];
+			res.forEach(function (client) {
+				if (client.cid == channelId)
+					targetClients.push(client);
+			});
+			callback(null, targetClients);
+		});
+	},
+	getChannelsByName: function (channelName, callback) {
+		this.__sendCommand("channelfind", { pattern: channelName }, function (err, res) {
+			return callback(err, res);
+		});
+	},
+	moveClient: function (clientId, targetChannelId, callback) {
+		this.__sendCommand("clientmove", { clid: clientId, cid: targetChannelId }, function (err, res) {
+			return callback(err, res);
+		});
+	},
+	kickClientFromServer: function (clientId, reason, callback) {
+		this.__sendCommand("clientkick", { clid: clientId, reasonid: 5, reasonmsg: reason }, function (err, res) {
+			if (typeof callback == "function")
+				return callback(err, res);
+		});
+	},
+	getClientById: function (clientId, callback) {
+		this.__sendCommand("clientinfo", { clid: clientId }, function (err, res) {
+			return callback(err, res);
+		});
+	},
+	getClientsByName: function (clientName, callback) {
+		this.__sendCommand("clientfind", { pattern: clientName }, function (err, res) {
+			return callback(err, res);
+		});
+	},
 	__login: function (username, password, callback) {
 		this.__sendCommand("login", { client_login_name: username, client_login_password: password }, function (err) {
 			if (err && typeof callback == "function")
