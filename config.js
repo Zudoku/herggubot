7939098bab376c57cb800bf84550f6ecd9aa79bf
Arttu Siren <arttu.siren@gmail.com>
2015-10-02 15:58:06 +0000

    config.js created online with Bitbucket

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {};
+config.VIRTUAL_SERVER_ID = 1;
+config.TS_IP = "";
+config.DATABASE_PATH = 'botdatabase';
+
+config.USERNAME = "";
+config.PASSWORD = "";
+config.QUERY_TIME_LIMIT = 1;
+config.NICKNAME = "HerGGuBot (Dev)";
+config.NETWORK_DEBUG = false;
+
+config.monitorChat = true;
+config.monitorChannelSlots = true;
+
+module.exports = config;
