42b79728e5b99f8314dfa531326a73367edf34a8
Arttu Siren <arttu.siren@gmail.com>
2015-10-12 14:46:25 +0000

    config.js edited online with Bitbucket

--- config.js
+++ config.js
@@ -1,15 +1,37 @@
 var config = {};
+
 config.VIRTUAL_SERVER_ID = 1;
 config.TS_IP = "";
 config.DATABASE_PATH = 'botdatabase';
-
-config.USERNAME = "";
-config.PASSWORD = "";
+config.SERVERQUERY_USERNAME = "";
+config.SERVERQUERY_PASSWORD = "";
 config.QUERY_TIME_LIMIT = 1;
-config.NICKNAME = "HerGGuBot (Dev)";
-config.NETWORK_DEBUG = false;
+config.NICKNAME = "HerGGuBot";
 
-config.monitorChat = true;
-config.monitorChannelSlots = true;
+config.DEBUG_NETWORK = false;
+config.launchBotOnStartUp = true;
+//------------------------------------
+// MODULES
+//------------------------------------
+config.module_monitor_chat = {
+	enabled: true,
+	spam_message : "Please do not spam the server chat.",
+	spam_limit: 4,
+	spam_timeframe: 5000
+};
+
+config.module_monitor_channel_slots = {
+	enabled: true,
+	channel_delete_time: 5000
+};
+
+config.module_extra_logs = {
+	enabled: true
+};
+
+config.module_web_interface = {
+	enabled: true,
+	port: 80
+};
 
 module.exports = config;
