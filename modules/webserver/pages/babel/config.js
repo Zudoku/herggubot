cccf89e1662c3865c641664487186c98b9e30e3f
PullaPoski <arttu.siren@gmail.com>
2015-10-20 21:44:37 +0300

    chunked log load, config and restart started. NOTE: this is buggy code, needs refactoring, pushing the update still

--- modules/webserver/pages/babel/config.js
+++ modules/webserver/pages/babel/config.js
@@ -9,20 +9,48 @@ var Config = React.createClass({
       cache: false,
       success: function(data) {
         this.setState({config : data});
+        $("#configTextArea")[0].value = JSON.stringify(this.state.config, null, ' ');
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
   },
+  uploadConfig: function(event) {
+    console.log($("#configTextArea")[0].value);
+    var configPayLoad = $("#configTextArea")[0].value;
+    var password = prompt("Password","");
+    $.ajax({
+      url: "/herggubot/api/uploadconfig",
+      dataType: 'json',
+      type: "POST",
+      data: {config: configPayLoad, pw: password},
+      success: function(data) {
+        if(data.success == true){
+          alert("Config uploaded! Restart the bot for it to take effect.");
+        }else{
+          alert("Config not uploaded correctly.");
+        }
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+        alert("Config not uploaded correctly.");
+      }.bind(this)
+    });
+  },
+  textAreaChanged : function(event){
+
+  },
   render: function() {
 
     return (
     	<div>
-      		<pre>
-        		{JSON.stringify(this.state.config, null, ' ')}
-      		</pre>
-      		Some values are hidden.
+      		<textarea id="configTextArea" className="form-control" rows="30" onChange={this.textAreaChanged} defaultValue={JSON.stringify(this.state.config, null, ' ')}>
+        		
+      		</textarea>
+      		Some values are hidden and can't be modified. Only JSON is accepted.
+          <hr/>
+          <button type="button" onClick={this.uploadConfig} className="btn btn-success">Save</button>
       	</div>
 
     );
