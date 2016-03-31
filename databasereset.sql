e209b3d04bce7a8bf1461266f3f1836d81945542
PullaPoski <arttu.siren@gmail.com>
2016-03-31 22:22:47 +0300

    reset database

---
+++ databasereset.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS ignorelist;
+DROP TABLE IF EXISTS serverchatlog;
+DROP TABLE IF EXISTS privatechatlog;
+DROP TABLE IF EXISTS actionlog;
+DROP TABLE IF EXISTS serveractionlog;
+DROP TABLE IF EXISTS errorlog;
+
+CREATE TABLE ignorelist
+(
+date datetime,
+databaseid INTEGER
+);
+
+CREATE TABLE serverchatlog
+(
+date datetime,
+text TEXT,
+sender TEXT,
+databaseid INTEGER
+);
+
+CREATE TABLE privatechatlog
+(
+date datetime,
+text TEXT,
+sender TEXT,
+databaseid INTEGER
+);
+
+CREATE TABLE actionlog
+(
+date datetime,
+text TEXT
+);
+
+CREATE TABLE serveractionlog
+(
+date datetime,
+text TEXT,
+actiontype TEXT
+);
+
+CREATE TABLE errorlog
+(
+date datetime,
+errormessage TEXT,
+reporter TEXT
+);
